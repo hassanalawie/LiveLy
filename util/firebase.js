5d29cdf5d4b2214bb0840ecc6a6bdeee5b596fd6
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2021-01-30 19:40:41 -0500

    firebase config

---
+++ util/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA5WLzSyEhtzwqHCCq_bYHyz8RUT5qHE4E",
+    authDomain: "lively-rtd.firebaseapp.com",
+    databaseURL: "https://lively-rtd-default-rtdb.firebaseio.com",
+    projectId: "lively-rtd",
+    storageBucket: "lively-rtd.appspot.com",
+    messagingSenderId: "227334457382",
+    appId: "1:227334457382:web:f658228aac6450413a7ee7",
+    measurementId: "G-CJVMN8Y3HE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
